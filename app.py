864e4f73a70a121e41ec785d608ee06df6f40a99
coresab <coreysaba@gmail.com>
2026-02-10 10:49:45 -0500

    Add   Dockerfile for Hugging Face deployment

--- app.py
+++ app.py
@@ -740,4 +740,6 @@ def update_projection_view(toggle_value, projection_data):
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=8051)
+    import os
+    port = int(os.environ.get("PORT", 7860))
+    app.run(debug=False, host="0.0.0.0", port=port)
